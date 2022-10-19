ea440056b9509c5c3e2cca8b4cb0ff23f6f8f857
unknown <Komal>
2022-10-19 23:45:51 +0530

    [Komal] Review File

---
+++ Product_Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    public class Product_Review
+    {
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+        public bool IsLike { get; set; }
+
+        //Method to override tostring for displaying assigned values
+        public override string ToString()
+        {
+            return $"Product Id : {ProductId}  \tUser Id : {UserId}  \tProduct Rating : {Rating}  \tProduct Review : {Review}    \tIsLike : {IsLike}";
+        }
+    }
+}
